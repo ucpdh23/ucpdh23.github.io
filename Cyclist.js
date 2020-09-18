f14e4ac6813ab96086a277a8705ca6265bbfb0fb
xan.xan@gmail.com <xan.xan@gmail.com>
2020-09-19 00:20:17 +0200

    energy

--- Cyclist.js
+++ Cyclist.js
@@ -78,8 +78,9 @@
                 y - this.velocity.y;
             text(((diffX > 0) ? ">>" : "<<") + " deltaX:" + diffX, 140, 30);
             text(((diffY < 0) ? "V" : "∆") + " deltaY:" + diffY, 140, 45);
-            text("vel:"+this.velocity.x*3600/1000, 140, 15);
+            text("vel:"+((int)(this.velocity.x*3600))/1000, 140, 15);
             text("diff:"+(globalFirst.position.x - this.position.x), 140, 60);
+            text("pulse:" + (int)(this.energy.pulse),240, 15)
         }
 
         // blue - cohesion
--- Energy.js
+++ Energy.js
@@ -1,10 +1,49 @@
 
 class Energy {
   constructor(cyclist) {
+    this.cyclist = cyclist;
     this.pulse = 80;
+    this.llano = 90;
+    this.refProp = 15 + this.llano / 10;
+    this.lastAcc = [0,0,0,0,0,0,0,0,0,0]
+    this.lastAccIndex = 0;
   }
   
   update(delta) {
+    var ms= this.cyclist.velocity.x * 3.6;
+    var prop = ms / this.refProp;
+    var g = prop * prop;
+    var newPulse = g * 21.0084034 +58.9915966;
     
+    if (newPulse +2 < this.pulse) {
+      this.pulse -= 0.3;
+    } else if (this.pulse +2<newPulse) {
+      this.pulse += 0.5;
+    } else {
+      this.pulse = newPulse;
+    }
+    
+    var accX = this.cyclist.acceleration.x;
+    if (accX < 0) accX = 0;
+    else accX = accX *5*delta;
+
+    var accVar = this.computeAccVar(accX);
+    this.pulse = this.pulse + accVar;
+    
+    
+    
+  }
+  
+  computeAccVar(acc) {
+    this.lastAcc[this.lastAccIndex] = acc;
+     var result = 0;
+    for (var i=0; i<this.lastAcc.length; i++) {
+      var index =(this.lastAccIndex + this.lastAcc.length) % this.lastAcc.length;
+      result += this.lastAcc[index] / (i +1)
+    }
+    
+    this.lastAccIndex = (this.lastAccIndex+1)%this.lastAcc.length;
+    
+    return result;
   }
 }
